ecf05fc3bbe0bc8a333e7358ccf441a584cf730a
unranked2 <109046701+unranked2@users.noreply.github.com>
2022-07-29 02:36:07 -0300

    Add files via upload

--- Storage.js
+++ Storage.js
@@ -56,20 +56,25 @@ function medida2(objeto, array, lego, ancho, divisionFloor) {
     return cantidadLargoTotal
 }
 
-function cargarColores(){
-    for (let i in auxColores){
-        let textocolor=`<option value="${auxColores[i]}">${auxColores[i]}</option>`
-        let color=document.getElementById('colorElegido')
-        color.innerHTML+= textocolor       
+function cargarColores() {
+    for (let i in auxColores) {
+        let textocolor = `<option value=${auxColores[i]}>${auxColores[i]}</option>`
+        let color = document.getElementById('colorElegido')
+        color.innerHTML += textocolor
     }
 }
 
-function crearNodo(padre,hijo,texto,clase){
+function crearNodo(padre, hijo, texto, clase) {
     let element = document.getElementById(padre)
     let newNodo = document.createElement(hijo)
-    newNodo.innerHTML=texto
-    newNodo.className=clase
+    newNodo.innerHTML = texto
+    newNodo.className = clase
     element.append(newNodo)
+}
+
+function actualizar() {
+    location.reload();
+    contador+=1
 }
 
 // Funciones de eventos
@@ -79,10 +84,10 @@ function eCalcular() {
     const altoReal = document.getElementById("alto").value
     const largoReal = document.getElementById("largo").value
 
-    while ((anchoReal<=0)||(altoReal<=0)||(largoReal<=0)||((anchoReal.length)=='')||((altoReal.length)=='')||((largoReal.length)=='')){
-        let texto1=`El valor ingresado no es correcto. Ingresá un número.<br>Para separar la parte decimal usa un punto`
-        document.getElementById("ingresa").innerHTML=texto1
-        document.getElementById("ingresa").style.color="orange";
+    while ((anchoReal <= 0) || (altoReal <= 0) || (largoReal <= 0) || ((anchoReal.length) == '') || ((altoReal.length) == '') || ((largoReal.length) == '')) {
+        let texto1 = `El valor ingresado no es correcto. Ingresá un número.<br>Para separar la parte decimal usa un punto`
+        document.getElementById("ingresa").innerHTML = texto1
+        document.getElementById("ingresa").style.color = "orange";
 
         anchoReal = document.getElementById("ancho").value
         altoReal = document.getElementById("alto").value
@@ -90,36 +95,36 @@ function eCalcular() {
 
     }
 
+    localStorage.setItem('anchovalue',anchoReal);
+    localStorage.setItem('altovalue', altoReal);
+    localStorage.setItem('largovalue', largoReal);
+
     nuevoAncho = conversorEscala(anchoReal)
     nuevoAlto = conversorEscala(altoReal)
     nuevoLargo = conversorEscala(largoReal)
 
-    document.getElementById("descubri").style.opacity="40%";
+    document.getElementById("descubri").style.opacity = "40%";
     document.getElementById("paso1").style.display = "none";
 
-    let texto1=`Las medidas de tu modelo serán:<br>${nuevoAncho}mm de ancho, ${nuevoAlto}mm de alto y ${nuevoLargo}mm de largo`
-    crearNodo("resultado1","p",`<p id="resultCalcular"></p>`,"resultados")
-    let nodoinfo=document.getElementById("resultCalcular")
-    nodoinfo.innerHTML=texto1
+    let texto1 = `Las medidas de tu modelo serán:<br>${nuevoAncho}mm de ancho, ${nuevoAlto}mm de alto y ${nuevoLargo}mm de largo`
+    crearNodo("resultado1", "p", `<p id="resultCalcular"></p>`, "resultados")
+    let nodoinfo = document.getElementById("resultCalcular")
+    nodoinfo.innerHTML = texto1
+
+    localStorage.setItem('medidasdelmodelo', texto1);
 
     document.getElementById("opcLego").removeAttribute('disabled')
     console.log("El evento eCalcular fue ejecutado")
-
-    localStorage.setItem('anchovalue', anchoReal);
-    localStorage.setItem('altovalue', altoReal);
-    localStorage.setItem('largovalue', largoReal);
-    localStorage.setItem('medidasdelmodelo', texto1);
-
 }
 
-function eSelect(e){
+function eSelect(e) {
 
     legoElegido = e.target.value;
 
-    let texto2=`${(mostrar(Lego, legoBasics, legoElegido))}`
-    crearNodo("selector1","div",`<p id="info"></p>`,"resultados")
-    let nodoinfo=document.getElementById("info")
-    nodoinfo.innerHTML=texto2
+    let texto2 = `${(mostrar(Lego, legoBasics, legoElegido))}`
+    crearNodo("selector1", "div", `<p id="info"></p>`, "resultados")
+    let nodoinfo = document.getElementById("info")
+    nodoinfo.innerHTML = texto2
 
     console.log("El evento eSelect fue ejecutado")
 
@@ -130,7 +135,7 @@ function eSelect(e){
 
 }
 
-function eConfirmar (e){
+function eConfirmar(e) {
 
     let lego = medida(Lego, legoBasics, legoElegido)
     const legoAncho = divisionFloor(nuevoAncho, lego)
@@ -139,104 +144,64 @@ function eConfirmar (e){
 
     let totalLaterales = (legoAncho * legoAlto * 2) + (legoLargo * legoAlto * 2)
     let totalTecho = (medida2(Lego, legoBasics, legoElegido, nuevoLargo, divisionFloor) * legoAncho)
-    
-    document.getElementById("paso2").style.display = "none";
-    document.getElementById("volver").style.visibility="visible";
 
-    let texto3=`Vas a necesitar aproximadamente ${totalLaterales} piezas para los laterales y ${totalTecho} para el techo. Para el suelo es necesario una BASE LEGO®.`
-    crearNodo("resultado2","p",`<p id="resultElegir"></p>`,"resultados")
-    let nodoinfo=document.getElementById("resultElegir")
-    nodoinfo.innerHTML=texto3
+    document.getElementById("paso2").style.display = "none";
+    document.getElementById("volver").style.visibility = "visible";
+
+    let texto3 = `Vas a necesitar aproximadamente ${totalLaterales} piezas para los laterales y ${totalTecho} para el techo. Para el suelo es necesario una BASE LEGO®.`
+    crearNodo("resultado2", "p", `<p id="resultElegir"></p>`, "resultados")
+    let nodoinfo = document.getElementById("resultElegir")
+    nodoinfo.innerHTML = texto3
 
     console.log("El evento eConfirmar fue ejecutado")
 
-    document.getElementById("fin").style.visibility="visible";
+    document.getElementById("fin").style.visibility = "visible";
 
     localStorage.setItem('infototal', texto3);
 }
 
-function eVolver(e){
+function eVolver(e) {
 
     e.target.removeEventListener('click', eVolver)
 
-    document.getElementById("volver").style.visibility="hidden";
+    document.getElementById("volver").style.visibility = "hidden";
 
     document.getElementById("paso1").style.display = "block";
     document.getElementById("paso2").style.display = "block";
     
-    document.getElementById("resultCalcular").remove()
-    document.getElementById("resultElegir").remove()
-    document.getElementById("fin").style.visibility="hidden";
+    document.getElementById("fin").style.visibility = "hidden";
 
     console.log("El evento eVolver fue ejecutado")
 
-    localStorage.removeItem('anchovalue');
-    localStorage.removeItem('altovalue');
-    localStorage.removeItem('largovalue');
-    localStorage.removeItem('medidasdelmodelo');
-    localStorage.removeItem('datoslego');
-    localStorage.removeItem('infototal');
-    localStorage.removeItem('color');
-    localStorage.removeItem('nombreprotvalue');
-    localStorage.removeItem('descripcionprotvalue');
+    localStorage.clear();
 }
 
-function eFinalizar(e){
-    
+function eFinalizar(e) {
+
     e.target.removeEventListener('click', eFinalizar)
 
-    
-    document.getElementById("fin").remove()
-    crearNodo("resultado2","p",`<hr>Felicitaciones, ya podés armar tu modelo`,"textofin")
 
-    document.getElementById("volver").remove()
-    
+    document.getElementById("fin").remove()
+    crearNodo("resultado2", "div", `<p id="fc"><hr>Felicitaciones, ya podés armar tu modelo</p>`, "textofin")
+
+    document.getElementById("volver").style.visibility = "hidden";
+
     console.log("El evento eFinalizar fue ejecutado")
 
-    setTimeout(()=>{
-        document.getElementById("guardarOno").style.visibility="visible";
+    setTimeout(() => {
+        document.getElementById("guardarOno").style.visibility = "visible";
     }, 500)
 
 }
 
-function actualizar(){
-    location.reload();
+function eRefresh() {
+    let f5 = document.getElementById("refresh")
+    f5.onclick = actualizar()
 }
 
-function eRefresh(){
-    let f5=document.getElementById("refresh")
-    f5.onclick=actualizar()
-}
-
-function eGuardar(e){
-    document.getElementById("caja1").style.display="none";
-    document.getElementById("caja2").style.display="block";
-}
-
-function eVerColec(){
-    document.getElementById("caja1").style.display="none";
-    document.getElementById("caja2").style.display="none";
-    document.getElementById("caja3").style.display="block";
-
-    let tituloMod=localStorage.getItem("nombreprotvalue")
-    crearNodo("modelox","p",tituloMod,"tituloMod")
-    let subTitle=localStorage.getItem("descripcionprotvalue")
-    crearNodo("modelox","p",subTitle,"subtexto1")
-    
-
-    // for (let i = 0; i < 10; i++) {
-    //     let clave = localStorage.key(i);
-    //     crearNodo("modelox","p",clave,"resultados")
-    // }
-
-}
-
-function eSelectColores(e){
-
-    let colorElegido = e.target.value;
-
-    localStorage.setItem('color', colorElegido);
-
+function eGuardar(e) {
+    document.getElementById("caja1").style.display = "none";
+    document.getElementById("caja2").style.display = "block";
 }
 
 function eRegistrarModelo() {
@@ -244,10 +209,10 @@ function eRegistrarModelo() {
     const nombreProt = document.getElementById("coleccion").value
     const descripcionProt = document.getElementById("descripcion").value
 
-    while (((nombreProt.length)=='')||((descripcionProt.length)=='')){
-        let texto1=`Por favor ingrese nombre y descripción`
-        document.getElementById("error").innerHTML=texto1
-        document.getElementById("error").style.color="orange";
+    while (((nombreProt.length) == '') || ((descripcionProt.length) == '')) {
+        let texto1 = `Por favor ingrese nombre y descripción`
+        document.getElementById("error").innerHTML = texto1
+        document.getElementById("error").style.color = "orange";
 
         nombreProt = document.getElementById("coleccion").value
         descripcionProt = document.getElementById("descripcion").value
@@ -258,14 +223,40 @@ function eRegistrarModelo() {
     localStorage.setItem('nombreprotvalue', nombreProt);
     localStorage.setItem('descripcionprotvalue', descripcionProt);
 
-    document.getElementById("colorElegido").addEventListener('change', eSelect)
+    // let miColor=""
+    // const colorElegido = document.getElementById("colorElegido")
+    // colorElegido.addEventListener('change', (e) => {
+    //     miColor= e.target.value
+    //     localStorage.setItem('color', miColor)
+    // })
 
-    document.getElementById("archiva").style.opacity="40%";
+    document.getElementById("archiva").style.opacity = "40%";
     document.getElementById("inputs2").style.display = "none";
 
     document.getElementById("guardarProt").remove()
-    crearNodo("boton3","p",`<hr>Añadiste un nuevo modelo a tu colección!`,"textofin")
+    crearNodo("boton3", "p", `<hr>Añadiste un nuevo modelo a tu colección!`, "textofin")
 
+    crearNodo("boton3", "div", `<button class="btn btn-primary" id="vercoleccion3">Ver colección</button>`, "col")
+    document.getElementById("vercoleccion3").addEventListener('click', eVerColec);
+
+}
+
+function eVerColec() {
+    document.getElementById("caja1").style.display = "none";
+    document.getElementById("caja2").style.display = "none";
+    document.getElementById("caja3").style.display = "block";
+
+    let textoModelo = `<em><br>${localStorage.getItem("descripcionprotvalue")}<br>Ancho: ${localStorage.getItem('anchovalue')}mts | Alto: ${localStorage.getItem('altovalue')}mts | Largo: ${localStorage.getItem('largovalue')}mts</em><br><br>${localStorage.getItem('medidasdelmodelo')}<br>${localStorage.getItem('datoslego')}<br>${localStorage.getItem('infototal')}`
+
+    crearNodo("coleccionados", "details", `<summary>${(localStorage.getItem("nombreprotvalue"))}</summary><p class="resultados2">${textoModelo}</p>`, "tituloMod")
+
+    //Resolver que no se sobreescriban los datos!
+
+    document.getElementById("recargar").addEventListener('click',eRepetir)
+}
+
+function eRepetir() {
+    location.reload(true);
 }
 
 // Construcción de objetos y arrays
@@ -279,7 +270,7 @@ class Lego {
     }
 }
 
-const legoBasics = [new Lego (1, "Lego 2x1", 16, 8), new Lego (2, "Lego 2x2", 16, 16), new Lego (3, "Lego 3x2", 24, 16), new Lego(4, "Lego 4x2", 32, 16), new Lego(5, "Lego 6x2", 48, 16),new Lego(6, "Lego 8x2", 64, 16)] 
+const legoBasics = [new Lego(1, "Lego 2x1", 16, 8), new Lego(2, "Lego 2x2", 16, 16), new Lego(3, "Lego 3x2", 24, 16), new Lego(4, "Lego 4x2", 32, 16), new Lego(5, "Lego 6x2", 48, 16), new Lego(6, "Lego 8x2", 64, 16)]
 
 class Colores {
     constructor(categoria, nombre) {
@@ -296,6 +287,7 @@ let nuevoAncho = 0
 let nuevoAlto = 0
 let nuevoLargo = 0
 let legoElegido = 0
+let contador = 0
 
 //Conversor de escalas
 document.getElementById("calcular").addEventListener('click', eCalcular);
@@ -318,13 +310,15 @@ document.getElementById("guardar").addEventListener('click', eGuardar);
 //Registro de modelo
 document.getElementById("guardarProt").addEventListener('click', eRegistrarModelo);
 
+//Retorno
+document.getElementById("recargar").addEventListener('click',eRepetir)
+
 //Colores
-let auxColores = []
-for (let i = 0; i < todosLosColores.length; i++) {auxColores.push(todosLosColores[i].nombre)}
-cargarColores()
+// let auxColores = []
+// for (let i = 0; i < todosLosColores.length; i++) {auxColores.push(todosLosColores[i].nombre)}
+// cargarColores()
 
 //Coleccion
 document.getElementById("vercoleccion1").addEventListener('click', eVerColec);
 document.getElementById("vercoleccion2").addEventListener('click', eVerColec);
-document.getElementById("vercoleccion3").addEventListener('click', eVerColec);
 
